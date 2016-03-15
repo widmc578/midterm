5af66da9ff9d2139f48d0bf3ff8d06b3c70011b3
Carolyn Widmer <carriewidmer@Carolyns-MBP.wireless.newschool.edu>
2016-03-15 13:30:27 -0400

    midterm

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$("#medieval").click(function(){
+	$("#more").removeClass("hide");
+	$("#medieval").hide();
+});
+
+
+$("#mechanical").click(function(){
+	$("#do").removeClass("it");
+	$("#mechanical").hide();
+});
+
+
+$("#more-images").click(function(){
+	$("#go").removeClass("forth");
+	$("#more-images").hide();
+});
